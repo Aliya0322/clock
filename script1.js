a5335e81050478342172a4eb8a25512c6d7dbb8b
Aliya <DusembekovaAZ@gmail.com>
2025-06-06 20:30:39 +0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,53 @@
+let apiTime = null;
+
+async function getTime(zoneName) {
+    const fetchResult = await fetch(`http://api.timezonedb.com/v2.1/get-time-zone?key=NDRUUY5TDIIA&format=json&by=zone&zone=${zoneName}`);
+    const data = await fetchResult.json();
+    const time = data.formatted.split(" ")[1];
+    const [hoursStr, minutesStr, secondsStr] = time.split(":");
+    const hours = parseInt(hoursStr, 10);
+    const minutes = parseInt(minutesStr, 10);
+    const seconds = parseInt(secondsStr, 10);
+
+    const nowDate = new Date();
+    nowDate.setHours(hours);
+    nowDate.setMinutes(minutes);
+    nowDate.setSeconds(seconds);
+    return nowDate;
+}
+
+async function arrowRun() {
+    let hours, minutes, seconds;
+    
+    if (apiTime instanceof Date) {
+        apiTime.setSeconds(apiTime.getSeconds() + 1);
+        hours = apiTime.getHours() % 12;
+        minutes = apiTime.getMinutes();
+        seconds = apiTime.getSeconds();
+    } else {
+        const now = new Date();
+        hours = now.getHours() % 12;
+        minutes = now.getMinutes();
+        seconds = now.getSeconds();
+    }
+
+    const secondsDeg = 6 * seconds;
+    const minutesDeg = 6 * minutes + secondsDeg / 60;
+    const hoursDeg = 30 * hours + (30 * seconds / 60 + 30 * minutes) / 60;
+    
+    document.getElementById('hour-arrow').style.transform = `rotate(${hoursDeg-90}deg)`;
+    document.getElementById('minute-arrow').style.transform = `rotate(${minutesDeg-90}deg)`;
+    document.getElementById('second-arrow').style.transform = `rotate(${secondsDeg-90}deg)`;
+}
+
+setInterval(arrowRun, 1000);
+
+document.getElementById('submit-btn').addEventListener('click', async function() {
+    const city = document.getElementById('city-input').value;
+    if (!city) {
+        alert('Введите название локацию верно');
+        return;
+    }
+    apiTime = await getTime(city);
+});
+
